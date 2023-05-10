117583fb3c5c7fa2c66a26d801a95ca2fa05ca42
Zeph <zephaniahsefuoduori@gmail.com>
2023-05-10 22:49:13 +0300

    Initial commit

---
+++ src/main/kotlin/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.springboot.blog.springbootblogrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootBlogRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootBlogRestApiApplication>(*args)
+}
